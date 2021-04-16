bc46bad2f4dbbb676eae5ed81669a29afab4dba1
red <purred@naver.com>
2021-04-16 18:28:52 +0900

    메인 화면 구성중

---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="posts-page">
+
+font-family: 'Noto Sans KR', sans-serif;
+
+    </div>
+</template>
+
+<style scoped>
+
+.posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
+
+</style>
